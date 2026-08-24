7d2fe5a7d5efb84cb1fb22342b3bf6a5463d8450
faiyaz73 <faiyazansari15901@gmail.com>
2026-08-24 01:27:08 -0700

    timeline

--- java.js
+++ java.js
@@ -1,43 +1,136 @@
-// import gsap from "gsap";
-gsap.to("#box1",{
-    x:1000,
-    duration:1,
-   rotate:360,
-   backgroundColor:"yellow",
-    delay:2,
-    borderRadius:"50%",
-    // stagger:1
+// // import gsap from "gsap";
+// gsap.to("#box1",{
+//     x:1000,
+//     duration:1,
+//    rotate:360,
+//    backgroundColor:"yellow",
+//     delay:2,
+//     borderRadius:"50%",
+//     // stagger:1
    
     
-})
-gsap.from("#box2",{
-    x:1000,
-    duration:3,
+// })
+// gsap.from("#box2",{
+//     x:1000,
+//     duration:3,
  
-    delay:5,
+//     delay:5,
    
     
-})
-gsap.to("#box3",{
-    x:1000,
+// })
+// gsap.to("#box3",{
+//     x:1000,
+//     duration:1,
+//    rotate:360,
+//    backgroundColor:"blue",
+//     delay:2,
+//     borderRadius:"50%",
+//     height:200,
+//     width:200,
+//     delay:2,
+//     yoyo:true,
+    
+//     // stagger:1
+   
+    
+// })
+
+// gsap.from("h1",{
+//     opacity:0,
+//     duration:2,
+//     y:400,
+//     stagger:0.5,   // one by one chalana h  to es propety ka use  krege
+// })
+
+
+// gsap.to("#box",{
+//     x:1000,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     borderRadius:"50%",
+//     repeat:-1,
+//     yoyo:true,
+//     ease:"power1.inOut",
+//     backgroundColor:"blue",
+//     border:"5px solid red",
+
+
+
+    
+    
+// })
+
+// gsap.to("#item1",{
+//     x:1500,
+//     duration:1,
+//     delay:1,
+//     // repeat:-1,
+//     // yoyo:true
+// })
+// gsap.to("#item2",{
+//     x:1500,
+//     duration:1,
+//     delay:2,
+//     // repeat:-1,
+//     // yoyo:true
+// })
+// gsap.to("#item3",{
+//     x:1500,
+//     duration:1,
+//     delay:3,
+//     // repeat:-1,
+//     // yoyo:true
+// })
+
+
+// timeline property  agr hme calculation  bachna h delay se related 
+
+
+const ti = gsap.timeline()
+
+ti.to("#item1",{
     duration:1,
-   rotate:360,
-   backgroundColor:"blue",
-    delay:2,
-    borderRadius:"50%",
-    height:200,
-    width:200,
-    delay:2,
-    yoyo:true,
-    
-    // stagger:1
-   
-    
+    x:1200,
+    delay:1,
+    rotate:360
 })
 
-gsap.from("h1",{
+ti.to("#item2",{
+    duration:1,
+    x:1200,
+   delay:1,
+   rotate:360   
+})
+ti.to("#item3",{
+    duration:1,
+    x:1200,
+   delay:1,
+   rotate:360 
+})
+
+
+let value = gsap.timeline()
+
+
+value.from("h1",{
+    y:-30,
+    duration:1,
     opacity:0,
-    duration:2,
-    y:400,
-    stagger:0.5,   // one by one chalana h  to es propety ka use  krege
+  
+})
+
+value.from("#box h2",{
+    y:-30,
+    opacity:0,
+    stagger:0.1,
+    duration:0.3,
+    stagger:1
+})
+
+value.from("h3",{
+    y:20,
+    opacity:0,
+    duration:0.3,
+    scale:0
 })
