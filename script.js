2a524fa28acbcadce8b1742a5ff63ead2066691d
faiyaz73 <faiyazansari15901@gmail.com>
2026-08-26 04:42:37 -0700

    scrub

--- script.js
+++ script.js
@@ -10,13 +10,52 @@ gsap.from("#page2 #box", {
     duration: 0.3,
     delay: 1,
     rotate:360,
-    scrollTrigger:"#page2 #box"
+    scrollTrigger:{
+        trigger:"#page2 #box",
+        scroller:"body",
+        markers:true,
+        start:"top 60%",
+        scrub:true      //  jaise jaise window upar niche krege animate hoga 
+    }
+    
 })
+
 gsap.from("#page3 #box", {
     scale:0,
     duration: 0.3,
     delay: 1,
     rotate:360,
-    scrollTrigger:"#page3 #box"
+    // scrollTrigger:"#page3 #box"
+    scrollTrigger:{
+         trigger:"#page3 #box",
+         scroller:"body",
+         markers:true,
+         start:"top 60%"
+    }}) 
 
+
+
+    gsap.from(" #page4 #boxinput h1",{
+    x:500,
+    opacity:0,
+    duration:2,
+    scrollTrigger:{
+        trigger:"#page4 #boxinput h1",
+        scroller:"body",
+        markers:true,
+        start:"top 60%"
+        
+    }
+})
+    gsap.from(" #page4 #boxinput h2",{
+    x:-500,
+    opacity:0,
+    duration:2,
+    scrollTrigger:{
+        trigger:"#page4 #boxinput h2",
+        scroller:"body",
+        markers:true,
+        start:"top 60%"
+        
+    }
 })
